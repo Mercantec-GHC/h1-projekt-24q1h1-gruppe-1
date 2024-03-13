13339f0dd17cc3b663fa571f4e8f0150e7382ec9
Sckaletz <allan@leeloo.dk>
2024-03-13 12:44:47 +0100

    Added display headset mechanism

--- Domain Models/Headset.cs
+++ Domain Models/Headset.cs
@@ -9,79 +9,92 @@ namespace Domain_Models
 {
     public abstract class Headset
     {
-        public Headset(string brand,string color,int frequencyRange,bool microphone,string typeOfConnection,bool noiseCancellation,string closeOrOpen,string itemCondition)
-        {
-            Brand = brand;
-            Color = color;
-            FrequencyRange = frequencyRange;
-            Microphone = microphone;
-            TypeOfConnection = typeOfConnection;
-            NoiseCancellation = noiseCancellation;
-            CloseOrOpen = closeOrOpen;
-            ItemCondition = itemCondition;
-        }
-        private string Brand;
+        public uint Id { get; set; }
+        private static uint lastId = 0;
+        public string Url { get; set; }
+        public string brand { get; set; }
+        public string model { get; set; }
+        public string color { get; set; }
+        public string frequencyRange { get; set; }
+        public bool microphone { get; set; }
+        public string typeOfConnection { get; set; }
+        public bool noiseCancellation { get; set; }
+        public string closeOrOpen { get; set; }
+        public string itemCondition { get; set; }
+        public string cableType { get; set; }
 
-        public string getBrand()
+        public Headset(string brand, string model, string color, string frequencyRange, bool microphone, string typeOfConnection, bool noiseCancellation, string closeOrOpen, string itemCondition)
         {
-            return Brand;
-        }
-
-        private string Color;
-        public string getColor()
-        {
-            return Color;
-        }
-
-        private int FrequencyRange;
-        public int getFrequencyRange()
-        {
-            return FrequencyRange;
-        }
-
-        private bool Microphone;
-        public bool getMicrophone()
-        {
-            return Microphone;
+            this.Url = $"headset/{lastId}";
+            Id = lastId++;
+            this.brand = brand;
+            this.model = model;
+            this.color = color;
+            this.frequencyRange = frequencyRange;
+            this.microphone = microphone;
+            this.typeOfConnection = typeOfConnection;
+            this.noiseCancellation = noiseCancellation;
+            this.closeOrOpen = closeOrOpen;
+            this.itemCondition = itemCondition;
         }
 
-        private string TypeOfConnection;
-        public string getTypeOfConnection()
+        public virtual void DisplayHeadset()
         {
-            return TypeOfConnection;
-        }
-
-        private bool NoiseCancellation;
-        public bool getNoiseCanellation()
-        {
-            return NoiseCancellation;
-        }
-
-        private string CloseOrOpen;
-        public string getCloseOrOpen()
-        {
-            return CloseOrOpen;
-        }
-
-        private string ItemCondition;
-        public string getItemCondition()
-        {
-            return ItemCondition;
-        }
-
-        public void getDetails()
-        {
-            Console.WriteLine("GetDetails");
+            Console.WriteLine($"{this.brand}");
+            Console.WriteLine($"{this.model}");
+            Console.WriteLine($"{this.color}");
+            Console.WriteLine($"{this.frequencyRange}");
+            Console.WriteLine($"{this.microphone}");
+            Console.WriteLine($"{this.typeOfConnection}");
+            Console.WriteLine($"{this.noiseCancellation}");
+            Console.WriteLine($"{this.closeOrOpen}");
+            Console.WriteLine($"{this.itemCondition}");
         }
     }
 
-    public class BluetoothHeadphones : Headset
+    public abstract class Headphones : Headset
     {
-        public BluetoothHeadphones(string brand, string color, int frequencyRange, bool microphone,
-            string typeOfConnection, bool noiseCancellation, string closeOrOpen, string itemCondition) 
-            : base (brand, color, frequencyRange, microphone, typeOfConnection, noiseCancellation, closeOrOpen, itemCondition)
+        public Headphones(string brand, string model, string color, string frequencyRange, bool microphone,
+           string typeOfConnection, bool noiseCancellation, string closeOrOpen, string itemCondition)
+             : base(brand, model, color, frequencyRange, microphone, typeOfConnection, noiseCancellation, closeOrOpen, itemCondition)
         {
 
         }
+    }
+
+    public class WiredHeadphones : Headphones
+    {
+        public string cableType { get; set; }
+        public WiredHeadphones(string brand, string model, string color, string frequencyRange, bool microphone,
+           string typeOfConnection, bool noiseCancellation, string closeOrOpen, string itemCondition)
+           : base(brand, model, color, frequencyRange, microphone, typeOfConnection, noiseCancellation, closeOrOpen, itemCondition)
+        {
+
+        }
+        public override void DisplayHeadset()
+        {
+            base.DisplayHeadset();
+            Console.WriteLine($"The cable is{this.cableType}");
+        }
+
+    }
+
+    public class BluetoothHeadphones : Headphones
+    {
+        public double bluetoothVersion { get; set; }
+
+        public BluetoothHeadphones(string brand, string model, string color, string frequencyRange, bool microphone,
+            string typeOfConnection, bool noiseCancellation, string closeOrOpen, string itemCondition)
+            : base(brand, model, color, frequencyRange, microphone, typeOfConnection, noiseCancellation, closeOrOpen, itemCondition)
+        {
+
+        }
+        public override void DisplayHeadset()
+        {
+            base.DisplayHeadset();
+            Console.WriteLine($"This headset comes with {bluetoothVersion}");
+        }
+
+
     }
 }
