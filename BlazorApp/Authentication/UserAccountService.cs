3c696490793410f50ba65d4a6f38084b6602d4a6
Sckaletz <allan@leeloo.dk>
2024-04-09 12:09:58 +0200

    Login/Logout working - Navbar updates!

--- BlazorApp/Authentication/UserAccountService.cs
+++ BlazorApp/Authentication/UserAccountService.cs
@@ -1,47 +1,32 @@
-﻿using Microsoft.AspNetCore.Components;
-using Npgsql;
+﻿using Npgsql;
 
 namespace BlazorApp.Authentication
 {
     public class UserAccountService
     {
-        private string? username { get; set; }
-        private string? userpassword { get; set; }
-        //private string ErrorMessage { get; set; } = "";
-
         private List<UserAccount> _users;
 
         public UserAccountService()
         {
             string connectionString = "Host=ep-blue-fire-a2nnh5p7.eu-central-1.aws.neon.tech;Username=sonaredb_owner;Password=8LlUVraRtDs9;Database=sonaredb;SSL Mode=Require";
+            _users = new List<UserAccount>();
 
-            using var connection = new NpgsqlConnection(connectionString);
-            connection.Open();
-
-            using (var cmd = new NpgsqlCommand())
+            using (var connection = new NpgsqlConnection(connectionString))
             {
-                cmd.Connection = connection;
+                connection.Open();
 
-                cmd.CommandText = @"
-                SELECT username, userpassword
-                FROM useraccount
-                WHERE username = @username AND userpassword = @userpassword";
-
-                cmd.Parameters.AddWithValue("@username", username);
-                cmd.Parameters.AddWithValue("@userpassword", userpassword);
-
-                using (var reader = cmd.ExecuteReader())
+                using (var cmd = new NpgsqlCommand("SELECT username, userpassword FROM useraccount", connection))
                 {
-                    while (reader.Read())
+                    using (var reader = cmd.ExecuteReader())
                     {
-                        string username = reader.GetString(reader.GetOrdinal("username"));
-                        string userpassword = reader.GetString(reader.GetOrdinal("userpassword"));
-
-                        _users = new List<UserAccount>
-
+                        while (reader.Read())
                         {
-                            new UserAccount{ UserName = username, Password = userpassword }
-                        };
+                            _users.Add(new UserAccount
+                            {
+                                UserName = reader.GetString(0),
+                                Password = reader.GetString(1)
+                            });
+                        }
                     }
                 }
             }
